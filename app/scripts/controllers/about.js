09bae763b0447513780d3a7464aba45942690a07
Nicholas Meyers <meyers.nicholas@gene.com>
2016-01-29 14:16:55 -0800

    yo angular generated

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name onionloaf2016App.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the onionloaf2016App
+ */
+angular.module('onionloaf2016App')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name onionloaf2016App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the onionloaf2016App
+ */
+angular.module('onionloaf2016App')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
